4b62fafde29b18ebd5247385a1a911049cf8b853
MiraclezzzS <131764763+MiraclezzzS@users.noreply.github.com>
2023-08-10 07:47:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,14 @@
 
 const express = require('express');
 const connectDB = require('./config/db');
-const cors = require('cors');
-
-// routes
-const books = require('./routes/api/books');
-const port = process.env.PORT || 8082;
 
 const app = express();
+const cors = require('cors');
+const books = require('./routes/api/books');
+// routes
 
 const path = require("path");
+
 process.env["NODE_CONFIG_DIR"] = path.join(path.resolve("./"), "config/");
 // Connect Database
 connectDB();
@@ -23,11 +22,10 @@ app.use(express.json({ extended: false }));
 
 app.get('/hello', (req, res) => res.send('Hello world!'));
 
-app.get("/test",(req,res) => res.send("test"));
 
 // use Routes
 app.use('/', books);
 
-
+const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
